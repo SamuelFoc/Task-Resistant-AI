12645a8c9152bc7be6a5286313f1cfccbc28d8d6
SamuelFoc <samo.sipikal@gmail.com>
2024-10-30 14:02:57 +0100

    Make tests

--- 8 - Strategy Tester/x-test.py
+++ 8 - Strategy Tester/x-test.py
@@ -1,25 +1,30 @@
 from lib.StrategyTester import StrategyTester
 
 # Configuration
-test_name = "test-7"
+test_name = "test-12"
 data_path = "data/clean_transactions.pq"
 split_date = '2019-10-01 00:00:00'
 target_col = "Is Fraud"
 datetime_col = "Datetime"
-num_rounds = 100
+apply_smote = True
+smote_sampling = 0.01
+model_type = "CatBoost"
+num_rounds = 75 
 model_params = {
-    'objective': 'binary:logistic',
-    'eval_metric': 'aucpr',
-    'learning_rate': 0.05,
-    'max_depth': 6,
-    'min_child_weight': 1,
-    'gamma': 4,
-    'subsample': 0.8,
-    'colsample_bytree': 0.8,
-    'scale_pos_weight': 30,
-    'tree_method': 'hist',
-    'device': 'cuda'
+    "iterations": 1000,  # Increased for better exploration of patterns
+    "learning_rate": 0.03,  # Reduced for finer adjustments per iteration
+    "depth": 8,  # Keep moderate depth, adjust based on tests
+    "loss_function": 'Logloss',
+    "eval_metric": 'AUC',
+    "verbose": 0,  # Print progress for better tracking
+    "random_state": 42,
+    "task_type": 'GPU',
+    "devices": '0',
+    "class_weights": [1, 10],  # Adjusted if true ratio differs
+    "early_stopping_rounds": 100,  # Stop early if no improvement
+    "l2_leaf_reg": 3,  # Regularization to prevent overfitting
 }
+
 evaluation_file = "evaluations.csv"
 
 # Initialize and run the StrategyTester
@@ -29,7 +34,9 @@ tester = StrategyTester(
     split_date=split_date,
     target_col=target_col,
     datetime_col=datetime_col,
-    model_type="XGBoost",
+    apply_smote=apply_smote,
+    smote_sampling_strategy=smote_sampling,
+    model_type=model_type,
     model_params=model_params,
     num_rounds=num_rounds,
     evaluation_file=evaluation_file
